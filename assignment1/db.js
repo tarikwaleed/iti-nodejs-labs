651eca9924dc414fb7b9e09ce3ade454871f94aa
tarikwaleed <takizee53@gmail.com>
2023-02-02 23:01:57 +0200

    done with assignment1 and assignment2 with :heart:

--- assignment1/db.js
+++ assignment1/db.js
@@ -1,5 +1,4 @@
-import { readFileSync, writeFileSync } from "fs"
-import { arrayBuffer } from "stream/consumers"
+const fs = require("fs")
 const METADATA_PATH =
   "/media/tarikwaleed/Data/iti/NODEJS/iti-nodejs-labs/assignment1/metadata.json"
 const DB_PATH =
@@ -13,36 +12,49 @@ function getCurrentId() {
   return parseInt(json.todo_count)
 }
 function readMetaDataFile() {
-  var metadata = readFileSync(METADATA_PATH)
+  var metadata = fs.readFileSync(METADATA_PATH)
   return JSON.parse(metadata)
 }
 function readDBFile() {
-  var data = readFileSync(DB_PATH)
+  var data = fs.readFileSync(DB_PATH)
   return JSON.parse(data)
 }
 function initializeDBFile() {
   // initialize the file with an empty array
   // passing those two extra parameters to stringify, makes it write the json PrettyPrinted
-  writeFileSync(DB_PATH, JSON.stringify([], null, 2))
+  fs.writeFileSync(DB_PATH, JSON.stringify([], null, 2))
 }
 function initializeMetaDataFile() {
   // passing those two extra parameters to stringify, makes it write the json PrettyPrinted
-  writeFileSync(METADATA_PATH, JSON.stringify({ todo_count: 0 }, null, 2))
+  fs.writeFileSync(METADATA_PATH, JSON.stringify({ todo_count: 0 }, null, 2))
 }
 function init() {
   initializeMetaDataFile()
   initializeDBFile()
 }
 function storeToDB(data) {
-  writeFileSync(DB_PATH, JSON.stringify(data, null, 2))
+  fs.writeFileSync(DB_PATH, JSON.stringify(data, null, 2))
 }
 function storeToMeta(data) {
-  writeFileSync(METADATA_PATH, JSON.stringify(data, null, 2))
+  fs.writeFileSync(METADATA_PATH, JSON.stringify(data, null, 2))
+}
+function reAssignIds() {
+  var i = 0
+  var json = readDBFile()
+  json.map((e) => (e.id = ++i))
+  storeToDB(json)
+}
+function prepareData(data) {
+  return data.reduce((prev, elem, index, arr) => {
+    const [key, value] = elem.split("=")
+    prev[key] = value
+    return prev
+  }, {})
 }
 
 function addTodo(todo) {
   //Read
-  var fileContent = readFileSync(DB_PATH)
+  var fileContent = fs.readFileSync(DB_PATH)
   //Parse
   var fileContentAsJSON = JSON.parse(fileContent)
   //Push
@@ -50,12 +62,12 @@ function addTodo(todo) {
   //Write
   try {
     // passing those two extra parameters to stringify, makes it write the json PrettyPrinted
-    writeFileSync(DB_PATH, JSON.stringify(fileContentAsJSON, null, 2))
+    fs.writeFileSync(DB_PATH, JSON.stringify(fileContentAsJSON, null, 2))
     var json = readMetaDataFile()
     json.todo_count += 1
     try {
       // passing those two extra parameters to stringify, makes it write the json PrettyPrinted
-      writeFileSync(METADATA_PATH, JSON.stringify(json, null, 2))
+      fs.writeFileSync(METADATA_PATH, JSON.stringify(json, null, 2))
       console.log("Todo added")
       reAssignIds()
     } catch (error) {
@@ -67,7 +79,7 @@ function addTodo(todo) {
 }
 function removeTodo(id) {
   if (id > 0 && id <= getCurrentId()) {
-    var data = readFileSync(DB_PATH)
+    var data = fs.readFileSync(DB_PATH)
     var json = JSON.parse(data)
     // var elementToBeDeleted = (e) => e.id == id;
     // var index = json.findIndex(elementToBeDeleted);
@@ -78,13 +90,11 @@ function removeTodo(id) {
     json.todo_count -= 1
     storeToMeta(json)
     reAssignIds()
-  } else console.log("Index out of range")
-}
-function reAssignIds() {
-  var i = 0
-  var json = readDBFile()
-  json.map((e) => (e.id = ++i))
-  storeToDB(json)
+    return 1
+  } else {
+    console.log("Index out of range")
+    return 0
+  }
 }
 function toggleTodo(id) {
   var json = readDBFile()
@@ -126,25 +136,17 @@ function editTodo(todo) {
   storeToDB(json)
 }
 
-function prepareData(data) {
-  return data.reduce((prev, elem, index, arr) => {
-    const [key, value] = elem.split("=")
-    prev[key] = value
-    return prev
-  }, {})
-}
-
 function main() {
   const [_, __, operation, ...data] = process.argv
   const options = prepareData(data)
+  var todo = {
+    id: getNextId(),
+    title: options.title,
+    content: options.content,
+    checked: false,
+  }
   switch (operation) {
     case "add":
-      var todo = {
-        id: getNextId(),
-        title: options.title,
-        content: options.content,
-        checked: false,
-      }
       addTodo(todo)
       break
     case "remove":
@@ -160,9 +162,17 @@ function main() {
       editTodo(options)
       break
     default:
-      console.log("unknown operation")
       break
   }
 }
 // init()
 main()
+
+module.exports = {
+  addTodo,
+  removeTodo,
+  editTodo,
+  toggleTodo,
+  listTodos,
+  getNextId,
+}
